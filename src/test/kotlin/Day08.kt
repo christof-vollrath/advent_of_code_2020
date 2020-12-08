b315b045936fd911ae6abdbe4795ff187dc3a6e1
Christof Vollrath <christof@taobits.net>
2020-12-08 22:42:25 +0100

    D08 - prepare part2

--- src/test/kotlin/Day08.kt
+++ src/test/kotlin/Day08.kt
@@ -1,3 +1,4 @@
+import io.kotest.assertions.fail
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -64,14 +65,63 @@ Immediately before the program would run an instruction a second time, the value
 Run your copy of the boot code.
 Immediately before any instruction is executed a second time, what value is in the accumulator?
 
+--- Part Two ---
+
+After some careful analysis, you believe that exactly one instruction is corrupted.
+
+Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp.
+(No acc instructions were harmed in the corruption of this boot code.)
+
+The program is supposed to terminate by attempting to execute an instruction
+immediately after the last instruction in the file.
+By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+
+For example, consider the same program from above:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+
+If you change the first instruction from nop +0 to jmp +0,
+it would create a single-instruction infinite loop, never leaving that instruction.
+If you change almost any of the jmp instructions,
+the program will still eventually find another jmp instruction and loop forever.
+
+However, if you change the second-to-last instruction (from jmp -4 to nop -4),
+the program terminates! The instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2
+jmp +4  | 3
+acc +3  |
+jmp -3  |
+acc -99 |
+acc +1  | 4
+nop -4  | 5
+acc +6  | 6
+
+After the last instruction (acc +6),
+the program terminates by attempting to run the instruction below the last instruction in the file.
+With this change, after the program terminates, the accumulator contains the value 8 (acc +1, acc +1, acc +6).
+
+Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp).
+What is the value of the accumulator after the program terminates?
+
  */
 
 class ConsoleCpu(val program: List<ConsoleCommand>) {
     var accu = 0
     var pc = 0
-    fun executeUntilLoop() {
+    fun execute() {
         val executed = mutableSetOf<Int>()
-        while(pc !in executed) {
+        while(pc < program.size) {
+            if (pc in executed) throw java.lang.IllegalArgumentException("Loop detected at $pc")
             executed += pc
             program[pc].execute(this)
         }
@@ -140,7 +190,10 @@ class Day08_Part1 : FunSpec({
     }
     context("execute program") {
         val console = ConsoleCpu(parseProgram(progString))
-        console.executeUntilLoop()
+        try {
+            console.execute()
+            fail("Loop should be detected")
+        } catch(e: java.lang.IllegalArgumentException) {}
         console.accu shouldBe 5
     }
 })
@@ -148,7 +201,10 @@ class Day08_Part1 : FunSpec({
 class Day08_Part1_Exercise: FunSpec({
     val input = readResource("day08Input.txt")!!
     val console = ConsoleCpu(parseProgram(input))
-    console.executeUntilLoop()
+    try {
+        console.execute()
+        fail("Loop should be detected")
+    } catch(e: java.lang.IllegalArgumentException) {}
     test("solution") {
         console.accu shouldBe 1859
     }
